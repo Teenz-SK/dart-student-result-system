acd6ee250175cdbe72d6b61c9cc87d245d175dd6
Teenz-SK <sanjukalshetti14@gmail.com>
2026-03-14 11:26:32 +0530

    Day 25 (14-03-2026): Implemented menu-driven Student Attendance System with default students, attendance marking, and present/absent count

---
+++ student_attendance_system/bin/student_attendance_system.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+void main() {
+
+  List<String> students = [
+    "Sanju",
+    "Rahul",
+    "Amit",
+    "Ravi",
+    "Kiran",
+    "Neha",
+    "Pooja",
+    "Arjun",
+    "Sneha",
+    "Rohit"
+  ];
+
+  List<String> attendance = List.filled(students.length, "Not Marked");
+
+  while (true) {
+
+    print("\n===== Student Attendance System =====");
+    print("1. Mark Attendance");
+    print("2. Show Attendance");
+    print("3. Exit");
+
+    print("Enter your choice:");
+
+    int choice = int.parse(stdin.readLineSync()!);
+
+    if (choice == 1) {
+
+      for (int i = 0; i < students.length; i++) {
+
+        print("\nStudent: ${students[i]}");
+        print("1. Present");
+        print("2. Absent");
+
+        int status = int.parse(stdin.readLineSync()!);
+
+        if (status == 1) {
+          attendance[i] = "Present";
+        }
+        else if (status == 2) {
+          attendance[i] = "Absent";
+        }
+        else {
+          print("Invalid choice");
+        }
+
+      }
+
+      print("\nAttendance marked successfully.");
+
+    }
+
+    else if (choice == 2) {
+
+      int presentCount = 0;
+      int absentCount = 0;
+
+      print("\n===== Attendance List =====");
+
+      for (int i = 0; i < students.length; i++) {
+
+        print("${students[i]} : ${attendance[i]}");
+
+        if (attendance[i] == "Present") {
+          presentCount++;
+        }
+        else if (attendance[i] == "Absent") {
+          absentCount++;
+        }
+
+      }
+
+      print("\nTotal Present: $presentCount");
+      print("Total Absent: $absentCount");
+
+    }
+
+    else if (choice == 3) {
+
+      print("Exiting program...");
+      break;
+
+    }
+
+    else {
+
+      print("Invalid choice.");
+
+    }
+
+  }
+
+}
---
+++ student_attendance_system/test/student_attendance_system_test.dart
@@ -0,0 +1,8 @@
+import 'package:student_attendance_system/student_attendance_system.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
